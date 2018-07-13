5cda824643518f324aaa72ff6a78e58fcb3d56b7
tafm <tafm@cin.ufpe.br>
2018-07-12 23:56:21 -0300

    Carregamento de arquivos de iluminação

--- projeto.ts
+++ projeto.ts
@@ -16,10 +16,10 @@ let canvas = $('#canvas')
 
 function loadCamera (arquivo : string) {
   let linhas : string[] = arquivo.split('\n')
-  let p1 = linhas[0].split(' ')
-  let p2 = linhas[1].split(' ')
-  let p3 = linhas[2].split(' ')
-  let p4 = linhas[3].split(' ')
+  let p1 : string[] = linhas[0].split(' ')
+  let p2 : string[] = linhas[1].split(' ')
+  let p3 : string[] = linhas[2].split(' ')
+  let p4 : string[] = linhas[3].split(' ')
 
   camera = new Camera(
     new Ponto3d(parseFloat(p1[0]), parseFloat(p1[1]), parseFloat(p1[2])),
@@ -32,7 +32,22 @@ function loadCamera (arquivo : string) {
 }
 
 function loadIluminacao (arquivo : string) {
+  let linhas : string[] = arquivo.split('\n')
+  let plAux: string[] = linhas[0].split(' ')
+  let pl : Ponto3d = new Ponto3d(parseFloat(plAux[0]), parseFloat(plAux[1]), parseFloat(plAux[2]))
+  let ka : number = parseFloat(linhas[1])
+  let iaAux : string[] = linhas[2].split(' ')
+  let ia : RGB = new RGB(parseInt(iaAux[0]), parseInt(iaAux[1]), parseInt(iaAux[2]))
+  let kd : number = parseFloat(linhas[3])
+  let odAux : string[] = linhas[4].split(' ')
+  let od : RGB = new RGB(parseInt(odAux[0]), parseInt(odAux[1]), parseInt(odAux[2]))
+  let ks : number = parseFloat(linhas[5])
+  let ilAux : string[] = linhas[6].split(' ')
+  let il : RGB = new RGB(parseInt(ilAux[0]), parseInt(ilAux[1]), parseInt(ilAux[2]))
+  let n : number = parseFloat(linhas[7])
 
+  iluminacao = new Iluminacao(pl, ka, kd, ks, n, ia, od, il)
+  console.log(iluminacao)
 }
 
 function loadObject (arquivo : string) { // carrega o arquivo de objetos (considera quebra de linha como \n)
@@ -76,6 +91,7 @@ function loadObject (arquivo : string) { // carrega o arquivo de objetos (consid
 }
 
 loadCamera('-200 -50 300\n0.667 0.172 -1\n0 3 0\n65 0.5 0.6')
+loadIluminacao('-200 -50 300\n1\n2 2 2\n1\n1 1 1\n0.5\n0 255 0\n2')
 loadObject('3 1\n50.0000 0.0000 0.000\n0 50 0\n0 0 50\n1 2 3')
 
 // console.log(new Ponto2d(1, 2))
